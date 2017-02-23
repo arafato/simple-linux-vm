ecda2a0de15efcb32ba1ea220d9332c1048748c9
arafato <oliver.arafat@outlook.com>
2017-02-23 14:11:25 +0100

    .

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 branch=$(git rev-parse HEAD)
 githubUser="arafato"
-githubProject="4t-arm"
+githubProject="simple-linux-vm"
 repoUrl="https://raw.githubusercontent.com/$githubUser/$githubProject/$branch/"
-resourceGroupName="fourt"
+resourceGroupName="playground"
 location="North Europe"
 azure group create -n "$resourceGroupName" -l "$location"
-azure group deployment create --debug-setting All --template-uri "$repoUrl/azuredeploy.json" --parameters "{\"repositoryUrl\":{\"value\":\"$repoUrl\"},\"projectPrefix\":{\"value\":\"fourt\"}}" -g "$resourceGroupName"
+azure group deployment create --debug-setting All --template-uri "$repoUrl/azuredeploy.json" --parameters "{\"repositoryUrl\":{\"value\":\"$repoUrl\"},\"projectPrefix\":{\"value\":\"arafato\"}}" -g "$resourceGroupName"
